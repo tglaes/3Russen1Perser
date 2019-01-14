35db039a20f0cf1eab1afe288ddc41db633341be
Mahan <mahandk128@gmail.com>
2019-01-14 21:47:32 +0100

    refactor the functions logout and getAllUser , add registation Window , add new styles to Login Window , add the navigation beetween the login and regisation sites , add checkEmailbyUser

--- D-Box/src/main/java/r1p/d/box/MyUI.java
+++ D-Box/src/main/java/r1p/d/box/MyUI.java
@@ -4,6 +4,7 @@ import javax.servlet.annotation.WebServlet;
 
 import com.vaadin.annotations.Theme;
 import com.vaadin.annotations.VaadinServletConfiguration;
+import com.vaadin.navigator.Navigator;
 import com.vaadin.server.VaadinRequest;
 import com.vaadin.server.VaadinServlet;
 import com.vaadin.ui.Button;
@@ -11,26 +12,35 @@ import com.vaadin.ui.Label;
 import com.vaadin.ui.TextField;
 import com.vaadin.ui.UI;
 import com.vaadin.ui.VerticalLayout;
+import com.vaadin.ui.themes.ValoTheme;
 import static java.lang.System.exit;
 import java.util.List;
 
 /**
- * This UI is the application entry point. A UI may either represent a browser window 
- * (or tab) or some part of an HTML page where a Vaadin application is embedded.
+ * This UI is the application entry point. A UI may either represent a browser
+ * window (or tab) or some part of an HTML page where a Vaadin application is
+ * embedded.
  * <p>
- * The UI is initialized using {@link #init(VaadinRequest)}. This method is intended to be 
- * overridden to add component to the user interface and initialize non-component functionality.
+ * The UI is initialized using {@link #init(VaadinRequest)}. This method is
+ * intended to be overridden to add component to the user interface and
+ * initialize non-component functionality.
  */
 @Theme("mytheme")
 public class MyUI extends UI {
 
+    protected Navigator navigator;
+    protected static final String REGISTATION_VIEW = "registration";
+    protected static final String LOGIN_VIEW = "";
+
     @Override
     protected void init(VaadinRequest vaadinRequest) {
-        final VerticalLayout layout = new VerticalLayout();
-        
-    LoginWindow test = new LoginWindow();
-    layout.addComponent(test);
+        navigator = new Navigator(this, this);
+        navigator.addView("", LoginFenster.class);
+        navigator.addView(REGISTATION_VIEW, RegistrationFenster.class);
     }
+
+    
+    
 
     @WebServlet(urlPatterns = "/*", name = "MyUIServlet", asyncSupported = true)
     @VaadinServletConfiguration(ui = MyUI.class, productionMode = false)
